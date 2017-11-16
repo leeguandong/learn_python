13e6d4c9bbc5ba559254b9b8675b6053177534c5
KUN <897158662@qq.com>
2017-11-16 13:39:38 +0800

    更新

--- Python核心编程/网络编程/网络编程概述  Socket.py
+++ Python核心编程/网络编程/网络编程概述  Socket.py
@@ -38,16 +38,59 @@
 # print('content is %s' % content)
 # print('content is %s' % content.decode('gb2312'))
 
-# 简单聊天室
+# # 简单聊天室
+# from socket import *
+#
+# def main():
+#     udpSocket = socket(AF_INET, SOCK_DGRAM)
+#     udpSocket.bind(('', 6678))
+#
+#     # 收到 打印
+#     recvInfo = udpSocket.recvfrom(1024)
+#     print('[%s]:%s' % (str(recvInfo[1]), recvInfo[0].decode('gb2312')))
+#
+# if __name__ == '__main__':
+#     main()
+
+# 这个聊天室是全双工的，两个线程一个接受一个发送
+from threading import Thread
 from socket import *
 
-def main():
-    udpSocket = socket(AF_INET, SOCK_DGRAM)
-    udpSocket.bind(('', 6678))
+# 1.收数据，然后打印
+def recvData():
+    while True:
+        recvInfo = udpSocket.recvfrom(1024)
+        print('>>%s:%s' % (str(recvInfo[1]), recvInfo[0]))
 
-    # 收到 打印
-    recvInfo = udpSocket.recvfrom(1024)
-    print('[%s]:%s' % (str(recvInfo[1]), recvInfo[0].decode('gb2312')))
+# 2.检测键盘，发数据
+def sendData():
+    while True:
+        sendInfo = input('<<')
+        udpSocket.sendto(sendInfo.encode('gb2312'), (destIp, destPort))
+
+udpSocket = None
+destIp = ''
+destPort = 0
+
+def main():
+    tr = Thread(target=recvData)
+    ts = Thread(target=sendData)
+
+    global udpSocket
+    global destIp
+    global destPort
+    destIp = input('请输入对方的ip')
+    destPort = input('请输入端口')
+
+    udpSocket = socket(AF_INET, SOCK_DGRAM)
+    # 发送的主机随机绑定ip地址，但是给一个4567端口
+    udpSocket.bind('', 4567)
+
+    tr.start()
+    ts.start()
+
+    tr.join()
+    ts.join()
 
 if __name__ == '__main__':
     main()
